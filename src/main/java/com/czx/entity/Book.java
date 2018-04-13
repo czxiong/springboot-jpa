1b0d4b9f285423175460139ee182c7c389b03bf0
czxiong <247859013@qq.com>
2018-04-13 15:30:04 +0800

    优化

--- src/main/java/com/czx/entity/Book.java
+++ src/main/java/com/czx/entity/Book.java
@@ -1,6 +1,7 @@
 package com.czx.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import java.util.Date;
 
 @Entity
 @Table(name = "book")
+@Data
 public class Book {
 
     @Id
@@ -22,46 +24,4 @@ public class Book {
 
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss",locale = "zh",timezone = "GMT+8")
     private Date publishTime;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public Date getPublishTime() {
-        return publishTime;
-    }
-
-    public void setPublishTime(Date publishTime) {
-        this.publishTime = publishTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price='" + price + '\'' +
-                ", publishTime=" + publishTime +
-                '}';
-    }
 }
